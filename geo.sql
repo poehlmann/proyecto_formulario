942b7547cebcb5693fd64c1dd506c7261be8e151
Bruno poehlmann <bruno.poehlmann@redeskbolivia.com>
2017-06-07 15:10:17 -0400

    ultimas modificaciones y actulizacion de la base de datos

--- geo.sql
+++ geo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 29-05-2017 a las 17:26:05
+-- Tiempo de generación: 07-06-2017 a las 21:08:49
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
@@ -23,42 +23,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `box`
+-- Estructura de tabla para la tabla `formulario`
 --
 
-CREATE TABLE `box` (
-  `id` int(11) NOT NULL,
-  `created_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `box`
---
-
-INSERT INTO `box` (`id`, `created_at`) VALUES
-(1, '2017-01-25 20:22:04'),
-(2, '2017-03-26 21:01:51'),
-(3, '2017-04-04 23:00:08');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `category`
---
-
-CREATE TABLE `category` (
+CREATE TABLE `formulario` (
   `id` int(11) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   `name` varchar(50) DEFAULT NULL,
   `description` text,
   `created_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
 --
--- Volcado de datos para la tabla `category`
+-- Volcado de datos para la tabla `formulario`
 --
 
-INSERT INTO `category` (`id`, `image`, `name`, `description`, `created_at`) VALUES
+INSERT INTO `formulario` (`id`, `image`, `name`, `description`, `created_at`) VALUES
 (3, NULL, 'geo01f01', NULL, '2017-05-28 21:29:12'),
 (4, NULL, 'geo01f02', NULL, '2017-05-28 21:29:51'),
 (5, NULL, 'geo01f03', NULL, '2017-05-28 21:29:57'),
@@ -73,36 +53,7 @@ INSERT INTO `category` (`id`, `image`, `name`, `description`, `created_at`) VALU
 (14, NULL, 'geo01f12', NULL, '2017-05-28 21:30:47'),
 (15, NULL, 'geo01f13', NULL, '2017-05-28 21:30:51'),
 (16, NULL, 'geo01f14', NULL, '2017-05-28 21:30:55'),
-(17, NULL, 'geo01f15', NULL, '2017-05-28 21:31:00'),
-(18, NULL, 'geo01f16', NULL, '2017-05-28 21:31:04');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `configuration`
---
-
-CREATE TABLE `configuration` (
-  `id` int(11) NOT NULL,
-  `short` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `kind` int(11) DEFAULT NULL,
-  `val` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `configuration`
---
-
-INSERT INTO `configuration` (`id`, `short`, `name`, `kind`, `val`) VALUES
-(1, 'title', 'Titulo del Sistema', 2, 'geo'),
-(2, 'use_image_product', 'Utilizar Imagenes en los productos', 1, '1'),
-(3, 'active_clients', 'Activar clientes', 1, '1'),
-(4, 'active_providers', 'Activar proveedores', 1, '1'),
-(5, 'active_categories', 'Activar categorias', 1, '1'),
-(6, 'active_reports_word', 'Activar reportes en Word', 1, '1'),
-(7, 'active_reports_excel', 'Activar reportes en Excel', 1, '1'),
-(8, 'active_reports_pdf', 'Activar reportes en PDF', 1, '1');
+(17, NULL, 'geo01f15', NULL, '2017-05-28 21:31:00');
 
 -- --------------------------------------------------------
 
@@ -113,14 +64,7 @@ INSERT INTO `configuration` (`id`, `short`, `name`, `kind`, `val`) VALUES
 CREATE TABLE `geo01f01` (
   `id` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
-  `correlativo` varchar(30) NOT NULL,
-  `nombre_instalador` varchar(30) NOT NULL,
-  `nombre_proyecto` varchar(30) NOT NULL,
-  `localizacion` varchar(30) NOT NULL,
-  `contrato_orden_trabajo` varchar(30) NOT NULL,
-  `sector_trabajo` varchar(30) NOT NULL,
-  `area_aceptada` tinyint(1) NOT NULL,
-  `fecha_proyecto` datetime NOT NULL,
+  `id_solicitud_form` int(11) NOT NULL,
   `aceptado_rechazado` varchar(11) NOT NULL,
   `fecha_aceptacion` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -129,42 +73,8 @@ CREATE TABLE `geo01f01` (
 -- Volcado de datos para la tabla `geo01f01`
 --
 
-INSERT INTO `geo01f01` (`id`, `id_usuario`, `correlativo`, `nombre_instalador`, `nombre_proyecto`, `localizacion`, `contrato_orden_trabajo`, `sector_trabajo`, `area_aceptada`, `fecha_proyecto`, `aceptado_rechazado`, `fecha_aceptacion`) VALUES
-(12, 1, 'prueba', 'prueba', 'prueba', 'prueba', 'prueba', 'prueba', 127, '2017-05-30 00:00:00', '1', '0000-00-00 00:00:00');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `operation`
---
-
-CREATE TABLE `operation` (
-  `id` int(11) NOT NULL,
-  `product_id` int(11) DEFAULT NULL,
-  `q` float DEFAULT NULL,
-  `operation_type_id` int(11) DEFAULT NULL,
-  `sell_id` int(11) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `operation_type`
---
-
-CREATE TABLE `operation_type` (
-  `id` int(11) NOT NULL,
-  `name` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `operation_type`
---
-
-INSERT INTO `operation_type` (`id`, `name`) VALUES
-(1, 'entrada'),
-(2, 'salida');
+INSERT INTO `geo01f01` (`id`, `id_usuario`, `id_solicitud_form`, `aceptado_rechazado`, `fecha_aceptacion`) VALUES
+(5, 1, 18, '1', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -174,16 +84,11 @@ INSERT INTO `operation_type` (`id`, `name`) VALUES
 
 CREATE TABLE `person` (
   `id` int(11) NOT NULL,
-  `image` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `lastname` varchar(50) DEFAULT NULL,
-  `company` varchar(50) DEFAULT NULL,
-  `address1` varchar(50) DEFAULT NULL,
-  `address2` varchar(50) DEFAULT NULL,
-  `phone1` varchar(50) DEFAULT NULL,
-  `phone2` varchar(50) DEFAULT NULL,
-  `email1` varchar(50) DEFAULT NULL,
-  `email2` varchar(50) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `phone` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
   `kind` int(11) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -192,8 +97,8 @@ CREATE TABLE `person` (
 -- Volcado de datos para la tabla `person`
 --
 
-INSERT INTO `person` (`id`, `image`, `name`, `lastname`, `company`, `address1`, `address2`, `phone1`, `phone2`, `email1`, `email2`, `kind`, `created_at`) VALUES
-(1, NULL, 'upsa', 'upsa', NULL, 'upsa', NULL, '7000000', NULL, 'upsa@gmail.com', NULL, 1, '2017-01-24 16:08:55');
+INSERT INTO `person` (`id`, `name`, `lastname`, `address`, `phone`, `email`, `kind`, `created_at`) VALUES
+(1, 'upsa', 'upsa', 'upsa', '7000000', 'upsa@gmail.com', 1, '2017-01-24 16:08:55');
 
 -- --------------------------------------------------------
 
@@ -219,8 +124,7 @@ CREATE TABLE `solicitud_form` (
 --
 
 INSERT INTO `solicitud_form` (`id`, `id_usuario`, `correlativo`, `nombre_instalador`, `nombre_proyecto`, `localizacion`, `contrato_orden_trabajo`, `sector_trabajo`, `area_aceptada`, `fecha_proyecto`) VALUES
-(14, 1, 'prueba', 'prueba', 'prueba', 'prueba', 'prueba', 'prueba', 200, '2017-05-30'),
-(15, 1, 'prueba', 'prueba', 'prueba', 'prueba', 'prueba', 'prueba', 200, '2017-05-30');
+(18, 1, 'prueba', 'prueba', 'prueba', 'prueba', 'prueba', 'prueba', 600, '2017-02-05');
 
 -- --------------------------------------------------------
 
@@ -230,12 +134,9 @@ INSERT INTO `solicitud_form` (`id`, `id_usuario`, `correlativo`, `nombre_instala
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) DEFAULT NULL,
-  `lastname` varchar(50) DEFAULT NULL,
+  `id_person` int(11) NOT NULL,
   `username` varchar(50) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
   `password` varchar(60) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
   `is_active` tinyint(1) NOT NULL DEFAULT '1',
   `is_admin` tinyint(1) NOT NULL DEFAULT '0',
   `created_at` datetime DEFAULT NULL
@@ -245,53 +146,26 @@ CREATE TABLE `user` (
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`id`, `name`, `lastname`, `username`, `email`, `password`, `image`, `is_active`, `is_admin`, `created_at`) VALUES
-(1, 'upsa', 'upsa', 'admin', 'admin', '90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad', NULL, 1, 1, '2017-01-24 14:58:12');
+INSERT INTO `user` (`id`, `id_person`, `username`, `password`, `is_active`, `is_admin`, `created_at`) VALUES
+(1, 1, 'admin', '90b9aa7e25f80cf4f64e990b78a9fc5ebd6cecad', 1, 1, '2017-01-24 14:58:12');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `box`
+-- Indices de la tabla `formulario`
 --
-ALTER TABLE `box`
+ALTER TABLE `formulario`
   ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `configuration`
---
-ALTER TABLE `configuration`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `short` (`short`),
-  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- Indices de la tabla `geo01f01`
 --
 ALTER TABLE `geo01f01`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `operation`
---
-ALTER TABLE `operation`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`),
-  ADD KEY `operation_type_id` (`operation_type_id`),
-  ADD KEY `sell_id` (`sell_id`);
-
---
--- Indices de la tabla `operation_type`
---
-ALTER TABLE `operation_type`
-  ADD PRIMARY KEY (`id`);
+  ADD UNIQUE KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_solicitud_form` (`id_solicitud_form`);
 
 --
 -- Indices de la tabla `person`
@@ -303,58 +177,40 @@ ALTER TABLE `person`
 -- Indices de la tabla `solicitud_form`
 --
 ALTER TABLE `solicitud_form`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_person` (`id_person`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `box`
+-- AUTO_INCREMENT de la tabla `formulario`
 --
-ALTER TABLE `box`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `category`
---
-ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
---
--- AUTO_INCREMENT de la tabla `configuration`
---
-ALTER TABLE `configuration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `formulario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 --
 -- AUTO_INCREMENT de la tabla `geo01f01`
 --
 ALTER TABLE `geo01f01`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
---
--- AUTO_INCREMENT de la tabla `operation`
---
-ALTER TABLE `operation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `operation_type`
---
-ALTER TABLE `operation_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT de la tabla `person`
 --
 ALTER TABLE `person`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT de la tabla `solicitud_form`
 --
 ALTER TABLE `solicitud_form`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
@@ -365,12 +221,23 @@ ALTER TABLE `user`
 --
 
 --
--- Filtros para la tabla `operation`
+-- Filtros para la tabla `geo01f01`
 --
-ALTER TABLE `operation`
-  ADD CONSTRAINT `operation_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  ADD CONSTRAINT `operation_ibfk_2` FOREIGN KEY (`operation_type_id`) REFERENCES `operation_type` (`id`),
-  ADD CONSTRAINT `operation_ibfk_3` FOREIGN KEY (`sell_id`) REFERENCES `sell` (`id`);
+ALTER TABLE `geo01f01`
+  ADD CONSTRAINT `geo01f01_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `geo01f01_ibfk_2` FOREIGN KEY (`id_solicitud_form`) REFERENCES `solicitud_form` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `solicitud_form`
+--
+ALTER TABLE `solicitud_form`
+  ADD CONSTRAINT `solicitud_form_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_person`) REFERENCES `person` (`id`) ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
